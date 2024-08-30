b4e05e4edc48c8f583654c7313e7fc386635e3c6
shaiksha <shaiknaveed5811@gmail.com>
2024-08-30 11:17:01 +0530

    methods file

--- Methods.java
+++ Methods.java
@@ -1,5 +1,19 @@
 package com.files;
 
-public class Methods {
+import java.util.Scanner;
+
+public class methods {
+	
+          Scanner sc=new Scanner(System.in);
+	  public void emp_wage() {
+		  System.out.println("Enter the Employee name : ");
+      	String nm=sc.next();
+               int pr = 18;
+              int hr = 4; 
+              float wage = pr * hr;
+              System.out.println(nm + " wage is: " + wage);
+              System.out.println();
+	    	
+	    }
 
 }
